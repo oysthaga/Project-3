c7b977cede23e5892e6f06e20cb6dfd383c4b96f
oysthaga <112075223+oysthaga@users.noreply.github.com>
2022-10-26 13:44:30 +0200

    Add files via upload

--- PenningTrap1.py
+++ PenningTrap1.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-
+"""
 v1Eu = pa.mat()
 r1Eu = pa.mat()
 v1Eu.load("v1Eu.bin")
@@ -31,7 +31,7 @@ r2RK_interacting = pa.mat()
 v2RK_interacting.load("v2RK_interacting.bin")
 r2RK_interacting.load("r2RK_interacting.bin")
 
-
+"""
 
 r1Eu0 = pa.mat()
 r1Eu1 = pa.mat()
@@ -49,28 +49,6 @@ r1RK0.load("r1_RK_0.bin")
 r1RK1.load("r1_RK_1.bin")
 r1RK2.load("r1_RK_2.bin")
 r1RK3.load("r1_RK_3.bin")
-
-
-
-# PARTICLES LEFT
-NumParticles0 = pa.mat()
-NumParticles1 = pa.mat()
-NumParticles2 = pa.mat()
-
-NumParticles0.load("NumParticles0.bin")
-NumParticles1.load("NumParticles1.bin")
-NumParticles2.load("NumParticles2.bin")
-
-
-omV = np.linspace(0.2,2.5,len(NumParticles0))
-
-plt.figure()
-plt.title('Particles left')
-plt.plot(omV, NumParticles0/NumParticles0[0], 'o', label='f = 0.1')
-plt.plot(omV, NumParticles1/NumParticles1[0], 'o', label='f = 0.4')
-plt.plot(omV, NumParticles2/NumParticles2[0], 'o', label='f = 0.7')
-plt.xlabel('$\omega_V [Hz]$'); plt.ylabel('Fraction of particles left')
-plt.legend()
 
 
 # COMPARING THE ANALYTICAL AND NUMERICAL SOLUTIONS 
@@ -92,6 +70,7 @@ phiminus = 0
 Aplus = (v0 + omminus*x0)/(omminus - omplus)
 Aminus = -(v0 + omplus*x0)/(omminus - omplus)
 
+
 t0 = np.linspace(0,50,np.shape(r1Eu0)[1]) 
 t1 = np.linspace(0,50,np.shape(r1Eu1)[1])
 t2 = np.linspace(0,50,np.shape(r1Eu2)[1])
@@ -109,6 +88,7 @@ f3 = Aplus*np.exp(-1j*(omplus*t3+phiplus)) + Aminus*np.exp(-1j*(omminus*t3+phimi
 r0 = np.array([f0.real, f0.imag, z0]); r1 = np.array([f1.real, f1.imag, z1])
 r2 = np.array([f2.real, f2.imag, z2]); r3 = np.array([f3.real, f3.imag, z3])
 
+"""
 errEu0 = np.linalg.norm((r0 - r1Eu0)/r0, axis=0)
 errEu1 = np.linalg.norm((r1 - r1Eu1)/r1, axis=0) 
 errEu2 = np.linalg.norm((r2 - r1Eu2)/r2, axis=0)
@@ -117,6 +97,15 @@ errRK0 = np.linalg.norm((r0 - r1RK0)/r0, axis=0)
 errRK1 = np.linalg.norm((r1 - r1RK1)/r1, axis=0)
 errRK2 = np.linalg.norm((r2 - r1RK2)/r2, axis=0)
 errRK3 = np.linalg.norm((r3 - r1RK3)/r3, axis=0)
+"""
+errEu0 = np.linalg.norm(r0 - r1Eu0, axis=0) / np.linalg.norm(r0, axis=0)
+errEu1 = np.linalg.norm(r1 - r1Eu1, axis=0) / np.linalg.norm(r1, axis=0)
+errEu2 = np.linalg.norm(r2 - r1Eu2, axis=0) / np.linalg.norm(r2, axis=0)
+errEu3 = np.linalg.norm(r3 - r1Eu3, axis=0) / np.linalg.norm(r3, axis=0)
+errRK0 = np.linalg.norm(r0 - r1Eu0, axis=0) / np.linalg.norm(r0, axis=0)
+errRK1 = np.linalg.norm(r1 - r1Eu1, axis=0) / np.linalg.norm(r1, axis=0)
+errRK2 = np.linalg.norm(r2 - r1Eu2, axis=0) / np.linalg.norm(r2, axis=0)
+errRK3 = np.linalg.norm(r3 - r1Eu3, axis=0) / np.linalg.norm(r3, axis=0)
 
 DeltaEu0 = max(np.linalg.norm(r0 - r1Eu0, axis=0)) 
 DeltaEu1 = max(np.linalg.norm(r1 - r1Eu1, axis=0))
@@ -135,6 +124,19 @@ rerrRK = (1/3)*(np.log(DeltaRK1/DeltaRK0) + np.log(DeltaRK2/DeltaRK1)
 
 print(f'r_(err, Euler) = {rerrEu}')
 print(f'r_(err, RK4) = {rerrRK}')
+
+"""
+# PARTICLES LEFT
+NumParticles0 = pa.mat()
+NumParticles1 = pa.mat()
+NumParticles2 = pa.mat()
+
+NumParticles0.load("NumParticles0.bin")
+NumParticles1.load("NumParticles1.bin")
+NumParticles2.load("NumParticles2.bin")
+
+
+omV = np.linspace(0.2,2.5,len(NumParticles0))
 
 # PLOT z AS A FUNCTION OF TIME
 N1 = np.shape(r1RK)[1]
@@ -156,7 +158,7 @@ plt.legend()
 plt.show()
 
 
-
+# PLOT xy-TRAJECTORY WITH INTERACTION
 plt.figure()
 plt.title("With interaction")
 plt.plot(r1RK_interacting[0,:], r1RK_interacting[1,:], label = "$P1$")
@@ -167,7 +169,7 @@ plt.axis('equal')
 plt.show()
 
 
-
+# PLOT v_x AS A FUNCTION OF x WITHOUT INTERACTION
 plt.figure()
 plt.title("Without interaction")
 plt.plot(r1RK[0,:], v1RK[0,:], label = "$P1$")
@@ -176,6 +178,7 @@ plt.xlabel("$x [\mu m]$"); plt.ylabel("$v_x [\mu m/ \mu s]$")
 plt.legend()
 plt.show()
 
+# PLOT v_z AS A FUNCTION OF z WITHOUT INTERACTION
 plt.figure()
 plt.title("Without interaction")
 plt.plot(r1RK[2,:], v1RK[2,:], label = "$P1$")
@@ -184,6 +187,7 @@ plt.xlabel("$z [\mu m]$"); plt.ylabel("$v_z [\mu m/ \mu s]$")
 plt.legend()
 plt.show()
 
+# PLOT v_x AS A FUNCTION OF x WITH INTERACTION
 plt.figure()
 plt.title("With interaction")
 plt.plot(r1RK_interacting[0,:], v1RK_interacting[0,:], label = "$P1$")
@@ -192,6 +196,7 @@ plt.xlabel("$x [\mu m]$"); plt.ylabel("$v_x [\mu m/ \mu s]$")
 plt.legend()
 plt.show()
 
+# PLOT v_z AS A FUNCTION OF z WITH INTERACTION
 plt.figure()
 plt.title("With interaction")
 plt.plot(r1RK_interacting[2,:], v1RK_interacting[2,:], label = "$P1$")
@@ -200,7 +205,7 @@ plt.xlabel("$z [\mu m]$"); plt.ylabel("$v_z [\mu m/ \mu s]$")
 plt.legend()
 plt.show()
 
-
+# PLOT 3D TRAJECTORY WITHOUT INTERACTION
 fig = plt.figure()
 ax1 = plt.axes(projection='3d')
 ax1.set_title("Without interaction")
@@ -209,6 +214,7 @@ ax1.plot(r2RK[0,:], r2RK[1,:], r2RK[2,:], label='$P2$')
 plt.legend()
 plt.show()
 
+# PLOT 3D TRAJECTORY WITH INTERACTION
 fig = plt.figure()
 plt.title("With interaction")
 ax2 = plt.axes(projection='3d')
@@ -217,9 +223,9 @@ ax2.plot(r1RK_interacting[0,:], r1RK_interacting[1,:], r1RK_interacting[2,:], la
 ax2.plot(r2RK_interacting[0,:], r2RK_interacting[1,:], r2RK_interacting[2,:], label='$P2$')
 plt.legend()
 plt.show()
+"""
 
-
-
+# PLOT RELATIVE ERROR BETWEEN ANALYTICAL AND NUMERICAL SOLUTION 
 plt.figure()
 plt.subplot(211)
 plt.title('Error Euler')
@@ -237,3 +243,24 @@ plt.plot(t2, errRK2, label='n3 = 16000')
 plt.plot(t3, errRK3, label='n4 = 32000')
 plt.xlabel('$t [\mu s]$'); plt.ylabel('Relative error')
 plt.legend()
+
+
+"""
+# PLOT PARTICLES LEFT
+plt.figure()
+plt.title('Particles left')
+plt.plot(omV, NumParticles0/NumParticles0[0], 'o', label='f = 0.1')
+plt.plot(omV, NumParticles1/NumParticles1[0], 'o', label='f = 0.4')
+plt.plot(omV, NumParticles2/NumParticles2[0], 'o', label='f = 0.7')
+plt.xlabel('$\omega_V [Hz]$'); plt.ylabel('Fraction of particles left')
+plt.legend()
+"""
+plt.figure()
+plt.plot(t0, r0[0])
+plt.plot(t3, r1RK3[0,:])
+plt.figure()
+plt.plot(t0, r0[1])
+plt.plot(t3, r1RK3[1,:])
+plt.figure()
+plt.plot(t0, r0[2])
+plt.plot(t3, r1RK3[2,:])
